84321ab9207e028814d5e6b10603176897f01bd3
NejcFrank <nf5039@student.uni-lj.si>
2022-01-04 11:50:14 +0100

    nova baza

--- web/Startup.cs
+++ web/Startup.cs
@@ -31,7 +31,7 @@ namespace web
 
 
             services.AddDbContext<ParkingContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("ParkingContext")));
+            options.UseSqlServer(Configuration.GetConnectionString("AzureContext")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>(options => options.Stores.MaxLengthForKeys = 128).
             AddEntityFrameworkStores<ParkingContext>().AddDefaultUI().AddDefaultTokenProviders();
